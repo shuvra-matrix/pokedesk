d6762fd709d5eee0027c12e30c25e25a2a3321a2
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-12-22 02:34:42 +0530

    basic struct done

---
+++ static/script.js
@@ -0,0 +1,18 @@
+const pokeContainer = document.querySelector(".poke-container");
+const pokemonCount = 150;
+const color = {
+  fire: "#FDDFDF",
+  grass: "#DFFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
